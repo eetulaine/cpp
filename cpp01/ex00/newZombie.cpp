d71e7434f1055aa7d58841605f27813b5d6464fb
eelaine <eelaine@student.hive.fi>
2025-06-18 11:04:27 +0300

    added try-catch to check for bad_alloc

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -6,12 +6,18 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 13:24:10 by eelaine           #+#    #+#             */
-/*   Updated: 2025/05/30 13:26:38 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/06/18 10:59:38 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* newZombie(std::string name) {
-	return (new Zombie(name));
+	try {
+		return new Zombie(name);
+	}
+	catch (const std::bad_alloc &e) {
+		std::cerr << "Memory allocation for zombie failed: " << e.what() << std::endl;
+		return nullptr;
+	}
 }
