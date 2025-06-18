a34e201c751c7c71a3bef4358890e91b3f269dca
eelaine <eelaine@student.hive.fi>
2025-06-18 15:39:13 +0300

    added FragTrap.hpp for cpp03/ex02

---
+++ cpp03/ex02/FragTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 15:11:24 by eelaine           #+#    #+#             */
+/*   Updated: 2025/06/18 15:27:55 by eelaine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+	
+	public:
+	
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(const FragTrap &copy);
+		~FragTrap();
+		FragTrap &operator=(const FragTrap &other);
+
+		void highFivesGuys();
+		void attack(const std::string &target);
+};
