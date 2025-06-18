0e2005b3bda6a02d4eaa3657f4ee6255fcbc5b5a
eelaine <eelaine@student.hive.fi>
2025-06-18 15:04:39 +0300

    created ScavTrap.hpp for cpp03/ex01

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 14:00:48 by eelaine           #+#    #+#             */
+/*   Updated: 2025/06/18 14:07:58 by eelaine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	
+	public:
+
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &copy);
+		~ScavTrap();
+		ScavTrap &operator=(const ScavTrap &other);
+		
+		void guardGate();
+		void attack(const std::string &target);
+};
