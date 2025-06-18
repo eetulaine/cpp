4e3e274066b9a0621dcc079de2e6655886d81a19
eelaine <eelaine@student.hive.fi>
2025-06-18 10:48:54 +0300

    switched to a more descriptive object name (raw to fixedPoint)

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,17 +6,17 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 11:21:53 by eelaine           #+#    #+#             */
-/*   Updated: 2025/06/13 11:48:44 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/06/16 11:40:18 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() : raw_(0) {
+Fixed::Fixed() : fixedPoint_(0) {
 	std::cout << "Default constructor called\n";
 }
 
-Fixed::Fixed(const Fixed& copy) : raw_(copy.raw_) {
+Fixed::Fixed(const Fixed &copy) : fixedPoint_(copy.fixedPoint_) {
 	std::cout << "Copy constructor called\n";
 }
 
@@ -24,20 +24,20 @@ Fixed::~Fixed() {
 	std::cout << "Destructor called\n";
 }
 
-Fixed & Fixed::operator = (const Fixed& copy) {
+Fixed& Fixed::operator = (const Fixed &copy) {
 	std::cout << "Copy assignment operator called\n";
 	if (this != &copy) {
-		raw_ = copy.raw_;
+		fixedPoint_ = copy.fixedPoint_;
 	}
 	return *this;
 }
 
 int	Fixed::getRawBits() const {
 	std::cout << "getRawbits called\n";
-	return raw_;
+	return fixedPoint_;
 }
 
-void Fixed::setRawBits(int const raw) {
+void Fixed::setRawBits(int const fixedPoint) {
 	std::cout << "setRawBits called\n";
-	raw_ = raw;
+	fixedPoint_ = fixedPoint;
 }
