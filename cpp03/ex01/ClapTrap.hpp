18440daf99dd14bfc8576f6bef72cca6ab2ca304
eelaine <eelaine@student.hive.fi>
2025-06-18 15:03:37 +0300

    created ClapTrap.hpp for cpp03/ex01

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 14:01:05 by eelaine           #+#    #+#             */
+/*   Updated: 2025/06/18 14:24:56 by eelaine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+class ClapTrap {
+
+    protected:
+        std::string name_;
+        int hitPoints_;
+        int energyPoints_;
+        int attackDamage_;
+    
+    public:
+		ClapTrap();
+        ClapTrap(std::string name);
+		ClapTrap(std::string name, int hp, int energy, int atk);
+        ClapTrap(const ClapTrap &copy);
+        ClapTrap &operator=(const ClapTrap &other);
+        ~ClapTrap();
+
+        void    attack(const std::string &target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+
+        void    setName(std::string name);
+        void    setHitPoints(int hitPoints);
+        void    setEnergyPoints(int energyPoints);
+        void    setAttackDamage(int attackDamage);
+
+        std::string getName() const;
+        int getHitPoints() const;
+        int getEnergyPoints() const;
+        int getAttackDamage() const;
+};
