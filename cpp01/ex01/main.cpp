ae345a0f5a18d2e9cd445ee1ca9b1f9a9920f86f
eelaine <eelaine@student.hive.fi>
2025-06-18 10:44:18 +0300

    removed return brackets

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,17 +6,17 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 13:55:26 by eelaine           #+#    #+#             */
-/*   Updated: 2025/05/30 14:34:03 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/06/18 10:36:50 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int	main() {
-	int	num_zombies = 2;
+	int	num_zombies = 99;
 
 	std::cout << "Zombie horde alert!\n";
 	Zombie* horde = zombieHorde(num_zombies, "Derek");
 	delete[] horde;
-	return (0);
+	return 0;
 }
