c184723005461d35ed47fd2546c37fb9ee59e3f4
eelaine <eelaine@student.hive.fi>
2025-06-25 11:33:50 +0300

    added new tests/objects

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 11:12:11 by eelaine           #+#    #+#             */
-/*   Updated: 2025/06/23 14:06:42 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/06/25 10:31:26 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ int main()
 {
 
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	const WrongAnimal* h = new WrongAnimal();
-	const WrongAnimal* cat = new WrongCat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << h->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	h->makeSound();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+	const WrongAnimal* wrong = new WrongAnimal();
+	const WrongAnimal* cat2 = new WrongCat();
+	std::cout << dog->getType() << " " << std::endl;
+	std::cout << cat->getType() << " " << std::endl;
+	std::cout << wrong->getType() << " " << std::endl;
+	dog->makeSound();
 	cat->makeSound();
+	meta->makeSound();
+	wrong->makeSound();
+	cat2->makeSound();
 
 	return 0;
 }
