9e6aaa99c08daf259ae8e7dbe33985af5de642d9
eelaine <eelaine@student.hive.fi>
2025-06-25 11:32:29 +0300

    added Brain functionality into Cat.cpp

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -6,26 +6,29 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:04:15 by eelaine           #+#    #+#             */
-/*   Updated: 2025/06/23 15:41:09 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/06/25 10:55:40 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
+#include "Brain.hpp"
 
-Cat::Cat() {
+Cat::Cat() : Animal("Cat") {
 	std::cout << "Cat default constructor called\n";
-	type_ = "Cat";
+	brain_ = new Brain();
 }
 
-Cat::Cat(const Cat &copy) {
+Cat::Cat(const Cat &copy) : Animal(copy) {
 	std::cout << "Cat copy constructor called\n";
-	type_ = copy.type_;
+	brain_ = new Brain(*copy.brain_);
 }
 
 Cat& Cat::operator=(const Cat &other) {
 	std::cout << "Cat copy assignment operator called\n";
 	if (this != &other) {
-		type_ = other.type_;
+		Animal::operator=(other);
+		delete brain_;
+		brain_ = new Brain(*other.brain_);
 	}
 	return *this;
 }
@@ -37,4 +40,18 @@ Cat::~Cat() {
 
 void	Cat::makeSound() const {
 	std::cout << type_ << ": *MEOWW*\n";
+}
+
+void	Cat::setIdea(unsigned int n, std::string idea) {
+	if (n > 99)
+		std::cout << "Cat has too many ideas!\n";
+	else
+		brain_->ideas_[n] = idea;
+}
+
+std::string Cat::getIdea(unsigned int n) {
+	if (n > 99)
+		return "Cat has too many ideas!\n";
+	else
+		return brain_->ideas_[n];
 }
