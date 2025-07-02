abc0028fa7125d21b67b32ced88297cad7542b3f
eelaine <eelaine@student.hive.fi>
2025-07-02 12:39:48 +0300

    rm'd unnecessary whitespace

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 15:00:45 by eelaine           #+#    #+#             */
-/*   Updated: 2025/06/06 15:06:43 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/07/02 12:32:44 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ HumanA::HumanA(std::string name, Weapon &weapon) : weapon_(weapon) {
 	this->name_ = name;
 }
 void HumanA::attack() {
-	std::cout << name_ << "  attacks with their " << weapon_.getType() << "\n";
+	std::cout << name_ << " attacks with their " << weapon_.getType() << "\n";
 }
