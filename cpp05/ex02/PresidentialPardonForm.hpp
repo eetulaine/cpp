b64c2cc3524db554a44caef10c145b26219e770b
eelaine <eelaine@student.hive.fi>
2025-07-03 14:45:09 +0300

    created first draft class

--- cpp05/ex02/PresidentialPardonForm.hpp
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,25 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:16:10 by eelaine           #+#    #+#             */
-/*   Updated: 2025/07/03 14:16:11 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/07/03 14:39:31 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+	
+	private:
+		
+		std::string target_;
+
+	public:
+	
+		PresidentialPardonForm();
+		PresidentialPardonForm(std::string target);
+		~PresidentialPardonForm();
+		PresidentialPardonForm(const PresidentialPardonForm &copy);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+	
+};
