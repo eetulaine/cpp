e0a7b7ed3887af2238d41b704dc22af66fe84dec
eelaine <eelaine@student.hive.fi>
2025-07-03 16:40:46 +0300

    added constructors

--- cpp05/ex02/ShrubberyCreationForm.cpp
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -6,13 +6,24 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:16:17 by eelaine           #+#    #+#             */
-/*   Updated: 2025/07/03 14:49:27 by eelaine          ###   ########.fr       */
+/*   Updated: 2025/07/03 16:38:24 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
-#include "AForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string target) {
-	
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("Shrubbery", 145, 137), target_("Default target") {}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm("Shrubbery", 145, 137), target_(target) {
+	std::cout << "Shrubbery Creation Form created\n";
 }
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &copy) : AForm(copy), target_(copy.target_) {}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other) {
+	if (this != &other)
+		target_ = other.target_;
+	return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
